f3d6a1eef85abb174d07e9b86e1635a48430f960
Ian Frizzell <ian.frizzell@gmail.com>
2021-03-21 11:57:48 -0600

    Added logging to main runner

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,9 @@
+import logging
 from waitress import serve
 from counts import app
+
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.INFO)
 
 if __name__ == '__main__':
     print('\x1b[6;30;42m' + 'starting wsgi server' + '\x1b[0m')
